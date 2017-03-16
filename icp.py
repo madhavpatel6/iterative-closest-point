c96d9cbd9983748d84f3135ed8872b22a5ee3fd4
Madhav Patel <patel.v.madhav@gmail.com>
2017-03-16 07:51:46 -0700

    added kdtree nearnest neighbor and updated readme

--- icp.py
+++ icp.py
@@ -6,15 +6,17 @@ from munkres import Munkres
 import random
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-
 import warnings
 import copy
 import os
+import time
+# for kdtree nearest neighbor
+import scipy.spatial
 # Ros specific
 import rospy
 from roslib import message
 import sensor_msgs.point_cloud2 as PCL
-from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import PointCloud2, PointField
 from roslib import message
 warnings.filterwarnings("ignore")
 
@@ -30,85 +32,94 @@ class MapBuilder:
         self.icp = IterativeClosestPoint(zero_threshold=0.1, convergence_threshold=0.0001)
         self.map = []
         self.new_scan = None
-        self.ros_listener_init()
+        rospy.init_node('listen', anonymous=True)
+        self.publisher = rospy.Publisher('/icp_map', PointCloud2, queue_size=10)
+        while(self.publisher.get_num_connections() == 0):
+            pass
+        self.subscriber = None
+        self.frame = None
 
+
+    def laser_listen_once(self):
+        self.subscriber = rospy.Subscriber('/cloud_out', PointCloud2, self.ros_callback)
 
     def ros_callback(self, data):
+        rospy.loginfo('Received new sensor data.')
+        self.subscriber.unregister()
+        self.frame = data.header.frame_id
         self.new_scan = self.pointcloud2_to_list(data)
-        print('New Scan Data:', self.new_scan)
-
-    def ros_listener_init(self):
-        rospy.init_node('listen', anonymous=True)
-        rospy.Subscriber('/cloud_out', PointCloud2, self.ros_callback)
 
     def pointcloud2_to_list(self, cloud):
         gen = PCL.read_points(cloud, skip_nans=True, field_names=('x', 'y', 'z'))
         list_of_tuples = list(gen)
-        return [list(elem) for elem in list_of_tuples]
+        #list_of_lists = [list(elem) for elem in list_of_tuples]
+        return list_of_tuples
+
+    def list_to_pointcloud2(self, points):
+        pcloud = PointCloud2()
+        pcloud = PCL.create_cloud_xyz32(pcloud.header, points)
+        pcloud.header.stamp = rospy.Time.now()
+        pcloud.header.frame_id = self.frame
+        return pcloud
+
+    def publish_map(self):
+        rospy.loginfo('Publishing ICP Map')
+        self.publisher.publish(self.list_to_pointcloud2(self.map))
 
     def build_map(self):
+        rospy.loginfo('Starting Map Building.')
+        self.laser_listen_once()
+        itr = 0
         while not rospy.is_shutdown():
             if self.new_scan is not None:
                 if len(self.map) != 0:
-                    new_scan_transformed = self.icp.iterative_closest_point(reference=self.map, source=self.new_scan, initial=[1, 0, 0, 0, 0, 0, 0],
+                    new_scan_transformed = self.icp.iterative_closest_point(self.map, source=self.new_scan, initial=[1, 0, 0, 0, 0, 0, 0],
                                                      number_of_iterations=10)
                     self.map.extend(new_scan_transformed)
                 else:
                     self.map = self.new_scan
-                    plot_points_2d(self.map, [])
-                    plt.pause(0.01)
+                if itr == 5:
+                    self.publish_map()
+                    itr = 0
+
                 self.new_scan = None
-                print('New Map', self.map)
+                self.laser_listen_once()
+                itr += 1
 
 
 
 class IterativeClosestPoint:
     def __init__(self, zero_threshold, convergence_threshold):
-        plt.ion()
         self.zero_threshold = zero_threshold
         self.convergence_threshold = convergence_threshold
         self.total_distances = []
-        self.debug_print = True
-        plt.figure(2)
-        plt.ylabel('Total Distance')
-        plt.xlabel('Iteration')
-        plt.grid()
-        plt.figure(1)
 
     def iterative_closest_point(self, reference, source, initial, number_of_iterations):
-        if self.debug_print is False:
-            sys.stdout = open(os.devnull, 'w')
         reference_n, source_n = reference, source
-        plt.figure(2)
-        plt.clf()
-        plt.figure(1)
         # Perform initial translation
         q = initial[0:4]
         rotation_matrix = self.compute_rotation_matrix(q)
         translation_vector = numpy.matrix(initial[4:7]).getT()
-        print('Performing initial transformation')
-        print('R =\n', rotation_matrix)
-        print('T =\n', translation_vector)
+        #print('Performing initial transformation')
+        #print('R =\n', rotation_matrix)
+        #print('T =\n', translation_vector)
 
-        plot_points_2d(reference_n, source_n)
-        plt.pause(.01)
         for i in range(len(source_n)):
             source_n[i] = list((rotation_matrix * numpy.matrix(source_n[i]).getT() + translation_vector).flat)
 
         for itr in range(number_of_iterations):
-            plt.figure(1)
-            plt.clf()
-            print('--------------------------------- Iteration # %d ---------------------------------' % (itr + 1))
-            reference_n, source_n, dist,  not_matched_reference, not_matched_source = self.nearest_neighbor_munkres(reference_n, source_n)
+            #print('--------------------------------- Iteration # %d ---------------------------------' % (itr + 1))
+#            reference_n, source_n, dist,  not_matched_reference, not_matched_source, multi_matched_index = self.nearest_neighbor_munkres(reference_n, source_n)
+            reference_n, source_n, dist, not_matched_reference, not_matched_source, multi_matched_index = self.nearest_neighbor_kdtree(reference_n, source_n)
             self.total_distances.append(dist)
-            print(reference_n)
-            print(source_n)
+            #print(reference_n)
+            #print(source_n)
 
             reference_mean = numpy.matrix(numpy.mean(reference_n, axis=0)).getT()
             source_mean = numpy.matrix(numpy.mean(source_n, axis=0)).getT()
 
-            print('Reference Center of Mass:\n', reference_mean)
-            print('Source Center of Mass   :\n', source_mean)
+            #print('Reference Center of Mass:\n', reference_mean)
+            #print('Source Center of Mass   :\n', source_mean)
 
             covariance_matrix = [[0 for i in range(len(reference_n[0]))] for j in range(len(reference_n[0]))]
 
@@ -118,16 +129,16 @@ class IterativeClosestPoint:
                 covariance_matrix += (source_std * reference_std)
 
             covariance_matrix *= (1 / len(reference_n))
-            print('Covariance Matrix:\n', covariance_matrix)
+            #print('Covariance Matrix:\n', covariance_matrix)
 
             anti_symmetric_matrix = covariance_matrix - covariance_matrix.getT()
-            print('Anti-symmetric matrix:\n', anti_symmetric_matrix)
+            #print('Anti-symmetric matrix:\n', anti_symmetric_matrix)
 
             column_vector = numpy.matrix([anti_symmetric_matrix[1, 2], anti_symmetric_matrix[2, 0], anti_symmetric_matrix[0, 1]]).getT()
-            print('Column vector:\n', column_vector)
+            #print('Column vector:\n', column_vector)
 
             covariance_entry = covariance_matrix + covariance_matrix.getT() - numpy.trace(covariance_matrix) * numpy.identity(3)
-            print('covariance entry\n', covariance_entry)
+            #print('covariance entry\n', covariance_entry)
 
             symmetric_matrix = numpy.matrix([
                 [numpy.trace(covariance_matrix), column_vector[0, 0], column_vector[1, 0], column_vector[2, 0]],
@@ -135,55 +146,45 @@ class IterativeClosestPoint:
                 [column_vector[1, 0], covariance_entry[1, 0], covariance_entry[1, 1], covariance_entry[1, 2]],
                 [column_vector[2, 0], covariance_entry[2, 0], covariance_entry[2, 1], covariance_entry[2, 2]]
             ])
-            print('symmetric matrix\n', symmetric_matrix)
+            #print('symmetric matrix\n', symmetric_matrix)
 
             e, v = numpy.linalg.eig(symmetric_matrix)
-            print('eigen values\n', e)
-            print('eigen vectors\n', v)
+            #print('eigen values\n', e)
+            #print('eigen vectors\n', v)
 
             max_eigenvalue_index = numpy.argmax(e)
-            print('max eigen value index', max_eigenvalue_index)
+            #print('max eigen value index', max_eigenvalue_index)
 
             q = list(numpy.matrix(v[:, max_eigenvalue_index]).flat)
-            print('max eigen vector\n', q)
+            #print('max eigen vector\n', q)
 
             rotation_matrix = self.compute_rotation_matrix(q)
-            print('rotation matrix\n', rotation_matrix)
+            #print('rotation matrix\n', rotation_matrix)
 
             translation_vector = reference_mean - rotation_matrix * source_mean
-            print('translation vector\n', translation_vector)
+            #print('translation vector\n', translation_vector)
             # Add in the not matched points
+            for i in sorted(multi_matched_index, reverse=True):
+                del reference_n[i]
             for p in not_matched_reference:
                 reference_n.append(p)
             for p in not_matched_source:
                 source_n.append(p)
-            plot_points_2d(reference_n, source_n)
             for i in range(len(source_n)):
                 source_n[i] = list((rotation_matrix*numpy.matrix(source_n[i]).getT() + translation_vector).flat)
-
-            self.plot_total_error()
             # Check convergence tolerance
             if dist < self.zero_threshold:
-                print('ICP total distance', dist, 'is below zero threshold')
+                #print('ICP total distance', dist, 'is below zero threshold')
                 break
             elif len(self.total_distances) >= 3:
-                print(self.total_distances)
+                #print(self.total_distances)
                 percent_difference1 = math.fabs((self.total_distances[-1] - self.total_distances[-2])/self.total_distances[-1])
                 percent_difference2 = math.fabs((self.total_distances[-1] - self.total_distances[-3])/self.total_distances[-1])
                 percent_difference3 = math.fabs((self.total_distances[-2] - self.total_distances[-3])/self.total_distances[-2])
                 if percent_difference1 < self.convergence_threshold and percent_difference2 < self.convergence_threshold and percent_difference3 < self.convergence_threshold:
-                    print('ICP converged to a total distance', dist)
+                    #print('ICP converged to a total distance', dist)
                     break
-            plt.figure(1)
-            plt.pause(0.1)
-            #plt.waitforbuttonpress()
-
-
-        #plt.waitforbuttonpress()
-        #plt.ioff()
         del self.total_distances[:]
-        if self.debug_print is False:
-            sys.stdout = sys.__stdout__
         return source_n
 
     def compute_rotation_matrix(self, q):
@@ -197,22 +198,22 @@ class IterativeClosestPoint:
         ])
 
     def nearest_neighbor_munkres(self, a, b):
-        matrix = [[0 for i in range(len(b))] for j in range(len(a))]
+        #print('Length: ', len(a), len(b))
+        rospy.loginfo('Matrix Dimensions: %d by %d', len(a), len(b))
+        matrix = [[self.compute_euclidean_distance_2d(a[i], b[j]) for j in range(len(b))] for i in range(len(a))]
 
-        for i in range(len(a)):
-            for j in range(len(b)):
-                distance = self.compute_euclidean_distance_2d(a[i], b[j])
-                matrix[i][j] = distance
+#        for i in range(len(a)):
+#            for j in range(len(b)):
+#                matrix[i][j] = self.compute_euclidean_distance_2d(a[i], b[j])):
 
-        print('First Point Set :', a)
-        print('Second Point Set:', b)
-        print(numpy.matrix(matrix))
         m = Munkres()
+        start = time.time()
         indexes = m.compute(matrix)
+        end = time.time()
+        rospy.loginfo('Munkres computation time: %0.3f', end - start)
         total = 0
         new_a = []
         new_b = []
-        plt.figure(1)
         # Create a set of indices, remove from the corresponding set when munkres has found a nearest neighbor
         # In the end the set will contain the index of any unmatched points
         not_matched_a_index = set(range(len(a)))
@@ -221,14 +222,13 @@ class IterativeClosestPoint:
         for row, column in indexes:
             value = matrix[row][column]
             total += value
-            print('(%d, %d)   -> %0.3f' % (row, column, value))
+            #print('(%d, %d)   -> %0.3f' % (row, column, value))
             new_a.append(a[row])
             new_b.append(b[column])
 
             not_matched_a_index.remove(row)
             not_matched_b_index.remove(column)
-            print('total cost: %0.3f' % total)
-            plt.annotate(s='', xy=new_a[-1][0:2], xytext=new_b[-1][0:2], arrowprops=dict(arrowstyle='<->'))
+            #print('total cost: %0.3f' % total)
 
         not_matched_a = []
         not_matched_b = []
@@ -236,9 +236,44 @@ class IterativeClosestPoint:
             not_matched_a.append(a[itr])
         for itr in not_matched_b_index:
             not_matched_b.append(b[itr])
-        print('Not matched A', not_matched_a)
-        print('Not matched B', not_matched_b)
-        return new_a, new_b, total, not_matched_a, not_matched_b
+        #print('Not matched A', not_matched_a)
+        #print('Not matched B', not_matched_b)
+
+        #rospy.loginfo("Time elapsed: " + str(end - start))
+        #Since munkres guarntees that it is matching without replacement we do not have to worry about multimatched indices
+        multi_matched_index = set()
+        return new_a, new_b, total, not_matched_a, not_matched_b, multi_matched_index
+
+    def nearest_neighbor_kdtree(self, a, b):
+        kdtree = scipy.spatial.cKDTree(a, leafsize=100)
+        cost = 0
+        new_a = []
+        new_b = []
+        not_matched_a_index = set(range(len(a)))
+        not_matched_b_index = set(range(len(b)))
+        multi_matched_index = set()
+        for i in range(len(b)):
+            new_cost, index = kdtree.query(b[i], k=1, distance_upper_bound=0.1)
+            # query will give a cost of infinite and an index = len(a) if no matches found within distance upper bound
+            if new_cost == math.inf and index == len(a):
+                continue
+            cost += new_cost
+            new_a.append(a[index])
+            new_b.append(b[i])
+            if index in not_matched_a_index:
+                not_matched_a_index.remove(index)
+            else:
+                multi_matched_index.add(len(new_a) - 1)
+            not_matched_b_index.remove(i)
+
+        not_matched_a = []
+        not_matched_b = []
+        for itr in not_matched_a_index:
+            not_matched_a.append(a[itr])
+        for itr in not_matched_b_index:
+            not_matched_b.append(b[itr])
+
+        return new_a, new_b, cost, not_matched_a, not_matched_b, multi_matched_index
 
     def compute_euclidean_distance_3d(self, a, b):
         return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2 + (a[2] - b[2])**2)
@@ -256,15 +291,15 @@ class IterativeClosestPoint:
 
 
 def test_icp():
-    print('Iteration Cloest Point')
+    #print('Iteration Cloest Point')
     inst = IterativeClosestPoint(zero_threshold=0.1, convergence_threshold=0.00000000001)
     p = read_coordinate_file('l_shape_3d.txt')
 
     x = read_coordinate_file('l_shape_3d.txt')
-    x = rotate_2d((0, 0), x, 75)
-    x = translate_2d(x, 5, 5)
-    del x[0]
+    x = rotate_2d((0, 0), x, 20)
+    x = translate_2d(x, -1, -1)
     inst.iterative_closest_point(p, x, [1, 0, 0, 0, 0, 0, 0], 100)
+
 
 
 def read_coordinate_file(filename):
@@ -282,7 +317,7 @@ def read_coordinate_file(filename):
             points.append([x, y])
         else:
             sys.exit('File format is incorrect.')
-    random.shuffle(points)
+    #random.shuffle(points)
     return points
 
 
@@ -312,6 +347,8 @@ def rotate_2d(origin, points, angle):
 
 
 def plot_points_2d(a, b):
+    plt.ion()
+    plt.clf()
     a_t = copy.deepcopy(a)
     b_t = copy.deepcopy(b)
     if len(a_t[0]) == 3:
@@ -324,6 +361,7 @@ def plot_points_2d(a, b):
     red = mpatches.Patch(color='red', label='Model Set')
     blue = mpatches.Patch(color='blue', label='Measured Set')
     plt.legend(handles=[red, blue])
+
 
 
 if __name__ == '__main__':
