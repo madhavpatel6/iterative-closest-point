0fe060bbe3db9d34bb787893f725da5937f71d8e
Madhav Patel <patel.v.madhav@gmail.com>
2017-03-21 23:58:31 -0700

    added images and subset optimization to icp

--- icp.py
+++ icp.py
@@ -17,6 +17,7 @@ import rospy
 from roslib import message
 import sensor_msgs.point_cloud2 as PCL
 from sensor_msgs.msg import PointCloud2, PointField
+from nav_msgs.msg import Odometry
 from roslib import message
 warnings.filterwarnings("ignore")
 
@@ -26,28 +27,43 @@ def main():
     m = MapBuilder()
     m.build_map()
 
+def get_subset_of_points(map, odom, radius):
+    subset = []
+    r2 = radius**2
+    for p in map:
+        if ((p[0] - odom[0])**2 + (p[1] - odom[1])**2 + (p[2] - odom[2])**2) <= r2:
+            subset.append(p)
+    return subset
 
 class MapBuilder:
     def __init__(self):
         self.icp = IterativeClosestPoint(zero_threshold=0.1, convergence_threshold=0.0001)
         self.map = []
-        self.new_scan = None
         rospy.init_node('listen', anonymous=True)
         self.publisher = rospy.Publisher('/icp_map', PointCloud2, queue_size=10)
-        while(self.publisher.get_num_connections() == 0):
-            pass
-        self.subscriber = None
+
+        self.new_odom = None
+        self.new_scan = None
+        self.cloud_out_subscriber = None
+        self.odom_subscriber = None
         self.frame = None
 
-
     def laser_listen_once(self):
-        self.subscriber = rospy.Subscriber('/cloud_out', PointCloud2, self.ros_callback)
+        self.cloud_out_subscriber = rospy.Subscriber('/cloud_out', PointCloud2, self.cloud_out_callback)
 
-    def ros_callback(self, data):
-        rospy.loginfo('Received new sensor data.')
-        self.subscriber.unregister()
+    def odom_listen_once(self):
+        self.odom_subscriber = rospy.Subscriber('/odom', Odometry, self.odom_callback)
+
+    def cloud_out_callback(self, data):
+        #rospy.loginfo('Received new sensor data.')
+        self.cloud_out_subscriber.unregister()
         self.frame = data.header.frame_id
         self.new_scan = self.pointcloud2_to_list(data)
+
+    def odom_callback(self, data):
+        #rospy.loginfo('Received new odom information.')
+        self.odom_subscriber.unregister()
+        self.new_odom = data
 
     def pointcloud2_to_list(self, cloud):
         gen = PCL.read_points(cloud, skip_nans=True, field_names=('x', 'y', 'z'))
@@ -63,29 +79,33 @@ class MapBuilder:
         return pcloud
 
     def publish_map(self):
-        rospy.loginfo('Publishing ICP Map')
+        #rospy.loginfo('Publishing ICP Map')
         self.publisher.publish(self.list_to_pointcloud2(self.map))
 
     def build_map(self):
         rospy.loginfo('Starting Map Building.')
         self.laser_listen_once()
+        self.odom_listen_once()
         itr = 0
+        oldtime = time.time()
         while not rospy.is_shutdown():
-            if self.new_scan is not None:
+            if self.new_scan is not None and self.new_odom is not None:
                 if len(self.map) != 0:
-                    new_scan_transformed = self.icp.iterative_closest_point(self.map, source=self.new_scan, initial=[1, 0, 0, 0, 0, 0, 0],
+                    #m = self.map
+                    pos = self.new_odom.pose.pose.position
+                    m = get_subset_of_points(map=self.map, odom=[pos.x, pos.y, pos.z], radius=20)
+                    new_scan_transformed = self.icp.iterative_closest_point(reference=m, source=self.new_scan, initial=[1, 0, 0, 0, 0, 0, 0],
                                                      number_of_iterations=10)
                     self.map.extend(new_scan_transformed)
                 else:
                     self.map = self.new_scan
-                if itr == 5:
-                    self.publish_map()
-                    itr = 0
-
                 self.new_scan = None
+                self.new_odom = None
                 self.laser_listen_once()
-                itr += 1
-
+                self.odom_listen_once()
+            if oldtime + 5 < time.time():
+                self.publish_map()
+                oldtime = time.time()
 
 
 class IterativeClosestPoint:
@@ -96,6 +116,7 @@ class IterativeClosestPoint:
 
     def iterative_closest_point(self, reference, source, initial, number_of_iterations):
         reference_n, source_n = reference, source
+
         # Perform initial translation
         q = initial[0:4]
         rotation_matrix = self.compute_rotation_matrix(q)
@@ -120,7 +141,6 @@ class IterativeClosestPoint:
 
             #print('Reference Center of Mass:\n', reference_mean)
             #print('Source Center of Mass   :\n', source_mean)
-
             covariance_matrix = [[0 for i in range(len(reference_n[0]))] for j in range(len(reference_n[0]))]
 
             for itr in range(len(reference_n)):
@@ -253,7 +273,7 @@ class IterativeClosestPoint:
         not_matched_b_index = set(range(len(b)))
         multi_matched_index = set()
         for i in range(len(b)):
-            new_cost, index = kdtree.query(b[i], k=1, distance_upper_bound=0.1)
+            new_cost, index = kdtree.query(b[i], k=1, distance_upper_bound=1)
             # query will give a cost of infinite and an index = len(a) if no matches found within distance upper bound
             if new_cost == math.inf and index == len(a):
                 continue
